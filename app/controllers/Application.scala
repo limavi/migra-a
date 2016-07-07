f3db06e8a7aa281a1a44086bd8bb5fc58f30df59
limavi2007 <limavi2007@hotmail.com>
2016-07-06 19:40:38 -0500

    modificacion

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,15 +4,25 @@ import javax.inject.Inject
 
 import autenticacion.modelo.Auth
 import autenticacion.servicios.AutServicios
+import migrana.modelo.{Episodio, Paciente,Repository}
+import migrana.services.migranaServices
 import play.api.libs.json._
 import play.api.mvc._
 import views.html
+
+import scala.concurrent.ExecutionContext.Implicits.global
 
 import scala.concurrent.Future
 
 class Application @Inject()(webJarAssets: WebJarAssets) extends Controller {
 
-  implicit val pacienteFormat = Json.format[Auth]
+  implicit val authFormat = Json.format[Auth]
+
+  import play.api.mvc._
+  import play.api.libs.json._
+
+  implicit val episodioFormat = Json.format[Episodio]
+  implicit val pacienteFormat = Json.format[Paciente]
 
   def autenticar: Action[JsValue] = Action.async( BodyParsers.parse.json ) { request =>
     request.body.validate[ Auth ] match {
@@ -30,4 +40,53 @@ class Application @Inject()(webJarAssets: WebJarAssets) extends Controller {
 
   def login = Action {Ok(html.login(webJarAssets))}
 
+  def consultarEpisodios(id: Option[ Long ]) = Action.async { implicit request =>
+    migranaServices.getEpisodios(id) map { episodio =>
+      Ok( Json.toJson( episodio ) )
+    }
+  }
+
+  def consultarEpisodiosPorPaciente(TipoDocumento: String, NumeroDocumento: Long)= Action.async { implicit request =>
+    migranaServices.getEpisodiosPorPaciente(TipoDocumento,NumeroDocumento) map { episodiosPorPaciente =>
+      Ok( Json.toJson( episodiosPorPaciente ) )
+    }
+  }
+
+  def consultarPacientes(TipoDocumento:Option[ String ],  NumeroDocumento: Option[Long ]): Action[AnyContent] = Action.async { implicit request =>
+    migranaServices.getPacientes(TipoDocumento,NumeroDocumento ) map { pacientes =>
+      Ok( Json.toJson( pacientes ) )
+    }
+  }
+
+  def agregarEpisodio= Action.async { implicit request =>
+    request.body.asJson.map { json =>
+      json.validate[Episodio].map{
+        case (episodio) => {
+          Repository.addEpisodio(episodio).map(resultado => {
+            //migranaServices.generarAlertaPorMuchosDolores(episodio.IdPaciente)
+            Ok("Episodio agregado satisfactoriamente")
+          })
+        }
+      }.recoverTotal{
+        e => Future(BadRequest("Existe un error en el JSON: "+ JsError.toFlatJson(e)))
+      }
+    }.getOrElse {
+      Future(BadRequest("Se esperaba un json para ejecutar el POST"))
+    }
+  }
+
+  def sincronizarEpisodios = Action { request =>
+    request.body.asJson.map { json =>
+      json.validate[List[Episodio]].map {
+        case (listEpisodios) => {
+          migranaServices.addListEpisodios(listEpisodios).map(resultado => println(resultado))
+          Ok("Episodios sincronizados satisfactoriamente")
+        }
+      }.recoverTotal {
+        e => BadRequest("Existe un error en el JSON: " + JsError.toFlatJson(e))
+      }
+    }.getOrElse {
+      BadRequest("Se esperaba un json para ejecutar el POST")
+    }
+  }
 }
